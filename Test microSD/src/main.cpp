b11898af57003e9d7c9e7a40a7e8acfa9b840399
AlexisTPS <alexis0boulay@gmail.com>
2019-12-12 15:54:09 +0100

    Implémentation d'une fonction d'écriture

--- Test microSD/src/main.cpp
+++ Test microSD/src/main.cpp
@@ -1,113 +1,64 @@
+/// Boulay Alexis ///
+/// Last entry 12 December 2019
+
 /*
-  SD card test
+  SD card read/write
 
- This example shows how use the utility libraries on which the'
- SD library is based in order to get info about your SD card.
- Very useful for testing a card when you're not sure whether its working or not.
+  This example shows how to read and write data to and from an SD card file
+  The circuit:
+   SD card attached to SPI bus as follows:
+ ** MOSI - pin 11
+ ** MISO - pin 12
+ ** CLK - pin 13
+ ** CS - pin 4 (for MKRZero SD: SDCARD_SS_PIN)
 
- The circuit:
-  * SD card attached to SPI bus as follows:
- ** MOSI - pin 11 on Arduino Uno/Duemilanove/Diecimila
- ** MISO - pin 12 on Arduino Uno/Duemilanove/Diecimila
- ** CLK - pin 13 on Arduino Uno/Duemilanove/Diecimila
- ** CS - depends on your SD card shield or module.
-         Pin 4 used here for consistency with other Arduino examples
+  created   Nov 2010
+  by David A. Mellis
+  modified 9 Apr 2012
+  by Tom Igoe
 
+  This example code is in the public domain.
 
- created  28 Mar 2011
- by Limor Fried
- modified 9 Apr 2012
- by Tom Igoe
- */
-// include the SD library:
-#include "SD.h"
+*/
+#define LED 7
+
 #include <SPI.h>
+#include <SD.h>
+#include <string.h>
 
-// set up variables using the SD utility library functions:
-Sd2Card card;
-SdVolume volume;
-SdFile root;
+File myFile;
 
-// change this to match your SD shield or module;
-// Arduino Ethernet shield: pin 4
-// Adafruit SD shields and modules: pin 10
-// Sparkfun SD shield: pin 8
-const int chipSelect = 10;
+// Permet d'inscrire une donnée sur la carte SD
+//// Impossibilité de récuperer plusieurs données en même temps
+template <class D> void writeonSD(File cartesd, const char* monfichier, D data) 
+{
+  cartesd=SD.open(monfichier, FILE_WRITE);
+  if (cartesd)
+  {
+    cartesd.println(data);
+    cartesd.close();
+  }
+}
+
 
 void setup() {
-  // Open serial communications and wait for port to open:
-  Serial.begin(9600);
-  while (!Serial) {
-    ; // wait for serial port to connect. Needed for native USB port only
+
+  // En cas de bug, la LED reste allumée
+  pinMode(LED,OUTPUT);
+  digitalWrite(LED, HIGH);
+  
+  if (!SD.begin(10)) {
+   // Indiquer que la carte est mal configurer (LED/Signal sonore tres agacant/serial pour les test)
+    while (1);
   }
+  // open the file. note that only one file can be open at a time,
+  // so you have to close this one before opening another.
+  writeonSD(myFile,"test.txt","J'écris donc je suis !"); //Attention ne fonction pas avec des tabs
+  writeonSD(myFile,"test.txt",6546);
 
-
-  Serial.print("\nInitializing SD card...");
-
-  // we'll use the initialization code from the utility libraries
-  // since we're just testing if the card is working!
-  if (!card.init(SPI_HALF_SPEED, chipSelect)) {
-    Serial.println("initialization failed. Things to check:");
-    Serial.println("* is a card inserted?");
-    Serial.println("* is your wiring correct?");
-    Serial.println("* did you change the chipSelect pin to match your shield or module?");
-    return;
-  } else {
-    Serial.println("Wiring is correct and a card is present.");
-  }
-
-  // print the type of card
-  Serial.print("\nCard type: ");
-  switch (card.type()) {
-    case SD_CARD_TYPE_SD1:
-      Serial.println("SD1");
-      break;
-    case SD_CARD_TYPE_SD2:
-      Serial.println("SD2");
-      break;
-    case SD_CARD_TYPE_SDHC:
-      Serial.println("SDHC");
-      break;
-    default:
-      Serial.println("Unknown");
-  }
-
-  // Now we will try to open the 'volume'/'partition' - it should be FAT16 or FAT32
-  if (!volume.init(card)) {
-    Serial.println("Could not find FAT16/FAT32 partition.\nMake sure you've formatted the card");
-    return;
-  }
-
-
-  // print the type and size of the first FAT-type volume
-  uint32_t volumesize;
-  Serial.print("\nVolume type is FAT");
-  Serial.println(volume.fatType(), DEC);
-  Serial.println();
-
-  volumesize = volume.blocksPerCluster();    // clusters are collections of blocks
-  volumesize *= volume.clusterCount();       // we'll have a lot of clusters
-  volumesize *= 512;                            // SD card blocks are always 512 bytes
-  Serial.print("Volume size (bytes): ");
-  Serial.println(volumesize);
-  Serial.print("Volume size (Kbytes): ");
-  volumesize /= 1024;
-  Serial.println(volumesize);
-  Serial.print("Volume size (Mbytes): ");
-  volumesize /= 1024;
-  Serial.println(volumesize);
-
-
-  Serial.println("\nFiles found on the card (name, date and size in bytes): ");
-  root.openRoot(volume);
-
-  // list all files in the card with date and size
-  root.ls(LS_R | LS_DATE | LS_SIZE);
-  pinMode(7, OUTPUT);
+  digitalWrite(LED,LOW);
 }
-void loop(void) {
-  digitalWrite(7,1);
-  delay(500);
-  digitalWrite(7,0);
-  delay(500);
+
+void loop() {
+  // nothing happens after setup
 }
